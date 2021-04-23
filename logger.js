5c2013ef6bb6ebb2ca1cc93b43a28fbfa80d6fa6
Lucas Gabriel <59308923+lllggghhhaaa@users.noreply.github.com>
2021-04-23 07:48:21 -0300

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,17 +1,39 @@
-const { createWriteStream } = require("fs")
+const { createWriteStream } = require("fs");
 
-exports.Log = async (message, type) => {
+exports.Log = async (message, type, dir) => {
     let logmessage;
-    var stream = createWriteStream(__dirname + "/log.txt", {flags:'a'});
+    var stream = createWriteStream(dir + "/log.txt");
+
+    const options = {
+        timeZone: 'America/Sao_Paulo',
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    };
+
+    const date = new Intl.DateTimeFormat([], options);
+    let time = (date.format(new Date()));
+
     switch(type)
     {
         case 0:
-            logmessage = `[Message] ${message}\n`
+            logmessage = `[${time}] [Message] ${message}`;
             break;
         case 1:
-            logmessage = `[Error] ${message}\n`
+            logmessage = `[${time}] [Warning] ${message}`;
+            break;
+        case 2:
+            logmessage = `[${time}] [Error] ${message}`;
             break;
     }
-    stream.write(logmessage)
-    console.log(message)
+    stream.write(logmessage + "\n");
+    console.log(logmessage);
+}
+exports.LogType = {
+    Message: 0,
+    Warning: 1,
+    Error: 2
 }
